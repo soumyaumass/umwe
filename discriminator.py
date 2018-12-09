5ac2fe54a7f0bfeda9fd356e028844dd9484fb58
soumyaumass <thesoumyasaha@gmail.com>
2018-12-09 18:59:45 -0500

    mistakes reverted

--- discriminator.py
+++ discriminator.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Nov 19 16:45:48 2018
-
-@author: Soumya
-"""
 import os
 import io
 import numpy as np
@@ -14,15 +8,17 @@ import torch.nn as nn
 class Discriminator(nn.Module):
     def __init__(self, lang):
         super(Discriminator, self).__init__()
-        
+
         self.lang = lang
         self.emb_dim = 300
         self.model = nn.Sequential(
-                nn.Linear(self.emb_dim, 128),
+                nn.Dropout(0.1),
+                nn.Linear(self.emb_dim, 2048),
                 nn.LeakyReLU(0.1),
-                nn.Linear(128, 32),
+                nn.Linear(2048, 2048),
                 nn.LeakyReLU(0.1),
-                nn.Linear(32, 1))
-        
+                nn.Linear(2048, 1))
+
     def forward(self, x):
-        return torch.sigmoid(self.model(x))
+        assert x.dim() == 2 and x.size(1) == self.emb_dim
+        return torch.sigmoid(self.model(x)).view(-1)
